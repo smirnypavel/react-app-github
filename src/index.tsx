d660899f4279b017b7e4dd26f6afe55ba1e00f59
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-07-23 23:45:23 +0300

    somme update

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/react-app-github">
+    <BrowserRouter basename="">
       <App />
     </BrowserRouter>
   </Provider>
